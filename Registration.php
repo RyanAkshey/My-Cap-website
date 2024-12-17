80de3e8965dce66c92b0f658042dc8944029fff1
Ryan Akshey Ansay <ryanansay123@gmail.com>
2024-12-17 20:02:17 +0800

    a

--- Registration.php
+++ Registration.php
@@ -4,12 +4,12 @@ include 'dbconn.php';  // Connect to the database
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Sanitize and validate the form data
-    $name = mysqli_real_escape_string($conn, $_POST["name"]);
+    $name = mysqli_real_escape_string($conn, $_POST["userName"]);
     $email = mysqli_real_escape_string($conn, $_POST["email"]);
     $password = password_hash($_POST["password"], PASSWORD_DEFAULT);  // Hash the password
 
     // Insert the user data into the database
-    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+    $sql = "INSERT INTO users (userName, email, password) VALUES ('$userName', '$email', '$password')";
   
     if ($conn->query($sql) === TRUE) {
         echo "Registration successful. <a href='userLog.html'>Login here</a>";
